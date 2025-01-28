99f9b5290f0cf25d779e78d2b0427ccef41c4069
cmacusb5 <cameron.macdonald-pownall@terumobct.com>
2025-01-28 11:58:25 -0700

    adding the next coding problem

---
+++ magicIndex/magicIndex.py
@@ -0,0 +1,6 @@
+# A magic index in an array A[1...n-1] is defined to be an index such that A[i] == i. 
+# Given a sorted array of distinc integers, write a method to find a magic index, if 
+# one exists, in array A
+
+A = [ -30, -20, -1, 1, 2, 3, 5, 7, 8, 12, 13]
+#idx   0    1   2   3  4  5  6  7  8  9   10
