873e5b407f221f55d484799d991fa03ba85ab5ae
cmacusb5 <cameron.macdonald-pownall@terumobct.com>
2025-04-11 14:00:23 -0600

    Today's code

--- circArray/circArray.py
+++ circArray/circArray.py
@@ -1,2 +1,45 @@
-# Implement a CircularArray class that supports and array-like data structure which 
+# Implement a CircularArray class that supports an array-like data structure which 
 # can be efficiently rotated. The class should be iterable.
+
+class CircularArray:
+   def clear(self):
+      self.iterable = [None] * self.ringSize
+   
+   def __init__(self, size: int):
+      self.ringSize = 10
+      self.clear()
+      self.readIdx = 0
+      self.writeIdx = 0
+
+   def _incrementWriteIdx(self, dir : int): #dir(ection): +1 for incrementing forward by 1, +2 for forward by 2, -1 for backwards by 1, etc
+      self.writeIdx += dir
+      if self.writeIdx >= self.ringSize:
+         self.writeIdx = 0
+      elif self.writeIdx < 0:
+         self.writeIdx = self.ringSize - 1
+      print(f'self.writeIdx = {self.writeIdx}')
+
+   def push(self, a):
+      self.iterable[self.writeIdx] = a
+      self._incrementWriteIdx(+1)
+
+   def pop(self):
+      item = self.iterable[self.writeIdx]
+      self.iterable[self.writeIdx] = None
+      self._incrementWriteIdx(-1)
+
+   def __iter__(self):
+      self.readIdx = 0
+      return self
+   
+   def __next__(self):
+      if (self.readIdx < len(self.iterable)):
+         item = self.iterable[self.readIdx]
+         self.readIdx += 1
+         return item
+      else:
+         raise StopIteration
+      
+def printIter(i):
+   for x in i:
+      print(x)
--- circArray/test_circArray.py
+++ circArray/test_circArray.py
@@ -1,1 +1,20 @@
 from circArray import *
+
+def test_list():
+   i1 = [1, 2, 3]
+   iterations = 0
+   for i in i1:
+      print(i)
+      iterations += 1
+   assert iterations == 3
+
+def test_iterable():
+   i1 = CircularArray(10)
+   length = sum(1 for i in i1)
+   
+   iterations = 0
+   for i in i1:
+         print(i)
+         iterations += 1
+   assert iterations == length
+
